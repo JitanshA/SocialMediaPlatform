320092424ab2f94c6668240597594305139d2c09
Jitansh Arora <arorajitansh32@gmail.com>
2023-11-15 22:24:51 -0400

    Finished basic backend

---
+++ server/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 25
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 25
+        },
+        email: {
+            type: String,
+            required: true,
+            max: 25,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 8,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 8,
+        },
+        picturePath: {
+            type: String,
+            default: "",
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        location: String,
+        viewedProfile: String,
+        impressions: Number
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
